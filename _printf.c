ae7bc5f8a081e4e1a21e47926a324d55769a385b
Jedid-Ahmed <jedidahmed88@gmail.com>
2022-11-17 14:35:25 +0300

    fix

--- _printf.c
+++ _printf.c
@@ -1,48 +1,48 @@
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
- * _printf - produces output according to a format
- * @format: format string containing the characters and the specifiers
- * Description: this function will call the get_print() function that will
- * determine which printing function to call depending on the conversion
- * specifiers contained into fmt
- * Return: length of the formatted output string
+ * _printf - prints anything
+ * @format: format of the number preceeding arguments to print
+ *
+ * Return: nothing
  */
+
 int _printf(const char *format, ...)
 {
-    int (*pfunc)(va_list, flags_t *);
-    const char *p;
-    va_list arguments;
-    flags_t flags = {0, 0, 0};
+	int i;
+	int index;
+	int (*routing)(va_list, char *, int);
 
-    register int count = 0;
+	char buffer[1024];
+	va_list ap;
 
-    va_start(arguments, format);
-    if (!format || (format[0] == '%' && !format[1]))
-        return (-1);
-    if (format[0] == '%' && format[1] == ' ' && !format[2])
-        return (-1);
-    for (p = format; *p; p++)
-    {
-        if (*p == '%')
-        {
-            p++;
-            if (*p == '%')
-            {
-                count += _putchar('%');
-                continue;
-            }
-            while (get_flag(*p, &flags))
-                p++;
-            pfunc = get_print(*p);
-            count += (pfunc)
-                         ? pfunc(arguments, &flags)
-                         : _printf("%%%c", *p);
-        }
-        else
-            count += _putchar(*p);
-    }
-    _putchar(-1);
-    va_end(arguments);
-    return (count);
+	i = 0;
+	index = 0;
+
+	va_start(ap, format);
+
+	while (format != NULL && format[i] != '\0')
+	{
+		if (format[i] != '%' && format[i - 1] != '%')
+		{
+			buffer[index] = format[i];
+			index++;
+		}
+		else if (format[i] == '%')
+		{
+			routing = router(format[i + 1]);
+			index = routing(ap, buffer, index);
+		}
+
+		i++;
+
+	}
+	va_end(ap);
+
+	write(1, buffer, index);
+	return (index);
 }
